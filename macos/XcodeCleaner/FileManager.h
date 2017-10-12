15d56520e7aa14174f1c530d167eaf0ca33ab887
waylybaye <havelove@gmail.com>
2017-10-12 15:03:34 +0800

    add AppIcon

---
+++ macos/XcodeCleaner/FileManager.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  XcodeCleaner
+//
+//  Created by Baye Wayly on 2017/10/12.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
---
+++ macos/XcodeCleaner/FileManager.m
@@ -0,0 +1,9 @@
+//
+//  FileManager.m
+//  XcodeCleaner
+//
+//  Created by Baye Wayly on 2017/10/12.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
